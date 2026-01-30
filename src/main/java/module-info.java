a5c456882da37166bb9a4c4cf8347b4eb95d9702
PabloDaniel0410 <pabmunqui2@alu.edu.gva.es>
2026-01-30 10:26:22 +0100

    compila he cambiado caosas pero dan errores raretess s

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module org.example.actividadfinaldi {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens org.example.actividadfinaldi to javafx.fxml;
+    opens org.example.actividadfinaldi.controller to javafx.fxml;
+    opens org.example.actividadfinaldi.model to javafx.base;
+
     exports org.example.actividadfinaldi;
-    exports org.example.actividadfinaldi.DAO;
-    opens org.example.actividadfinaldi.DAO to javafx.fxml;
+    exports org.example.actividadfinaldi.controller;
+    exports org.example.actividadfinaldi.model;
 }
